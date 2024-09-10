17026f7eb7bcbaa727cb193af9ad1e714ff1c1e9
mauriciomrds <66748150+mauriciomrds@users.noreply.github.com>
2024-09-10 17:32:13 -0300

    Add files via upload

---
+++ CursoJava/src/curso/java/adcio/EntradaDeDados.java
@@ -0,0 +1,24 @@
+
+package curso.java.adcio;
+
+import javax.swing.JOptionPane;
+
+public class EntradaDeDados {
+
+	public static void main(String[]args) {
+		
+String carros =	JOptionPane.showInputDialog("Informe a quantidade de carro");
+String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");
+
+double carroNumero = Double.parseDouble(carros);
+double pessoaNumero = Double.parseDouble(pessoas);
+
+int divisao = (int)(carroNumero /pessoaNumero);
+double resto = carroNumero % pessoaNumero;
+
+JOptionPane.showInternalMessageDialog(null, "Divisão para pessoas deu: "+ divisao + " carro e sobrou: "+ resto + " carros");		
+		
+	}
+	
+	
+}
