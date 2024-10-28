911aa1af059edf49c8b0e4679203b07e924ae1c8
lenovo-20 <lenovo-20@LAPTOP-VD6MJEJB>
2024-10-28 14:33:53 -0300

    Criação de Lista separadas

--- CursoJava/src/cursojava/executavel/SegundaClasseJava.java
+++ CursoJava/src/cursojava/executavel/SegundaClasseJava.java
@@ -7,14 +7,23 @@ import javax.swing.JOptionPane;
 
 import cursojava.classes.Aluno;
 import cursojava.classes.Disciplina;
+import cursojava.constantes.StatusAluno;
 
 public class SegundaClasseJava {
 
 	public static void main(String[] args) {
 		
+		//Criação lista alunos
 		List<Aluno>alunos = new ArrayList<Aluno>();
 		
-		   for(int qtd = 1; qtd <=2; qtd ++) {
+		//Criação de nova lista de alunos APROVADO, REPROVADO E RECUPERAÇÃO
+		List<Aluno>alunosAprovados = new ArrayList<Aluno>();
+		List<Aluno>alunosRecuperacao = new ArrayList<Aluno>();
+		List<Aluno>alunosReprovados = new ArrayList<Aluno>();
+		
+		//lista for por quantidade de alunos
+		   for(int qtd = 1; qtd <=5; qtd ++) {
+			   
 		
 		String nome = JOptionPane.showInputDialog("Informe o nome do aluno: "+ qtd + " ?");
 		/*String idade = JOptionPane.showInputDialog("Informe  idade do aluno: ");
@@ -57,7 +66,7 @@ public class SegundaClasseJava {
 	       aluno1.setSerieMatriculado(serie);*/
 	       
 	       
-	       for(int pos = 1 ; pos <= 4 ; pos ++) {
+	       for(int pos = 1 ; pos <= 1 ; pos ++) {
 	    	   
 	    	   String nomeDisciplina = JOptionPane.showInputDialog(" Nome da disciplina "+pos+"?" );
 	    	   String notaDisciplina = JOptionPane.showInputDialog(" Nota da disciplina "+pos+"?" );
@@ -85,25 +94,129 @@ public class SegundaClasseJava {
 	    	   }
 	       }
 	       
+	       
+	       //Adicionando aluno
 	       alunos.add(aluno1);
 	       
-	   }  
-		   for (Aluno aluno : alunos) {
+		   } 
+		   //Pecorrendo lista de alunos
+		/*   for (Aluno aluno : alunos) {
 			   
+			
+			   //Procurando um aluno e calculando a média
+			   if(aluno.getNome().equalsIgnoreCase("mauricio")){
+				   //Pecorrendo lista de aluno e removendo
+				   alunos.remove(aluno);
+				   break;
+			   
+			   }else {
 			   //Metodo toString
 	           System.out.println(aluno);
 	           System.out.println("Nota do Aluno1: "+ aluno.getMediaNota());
 		       System.out.println("Resultado: "+ aluno.getAlunoAprovado2());
 		     System.out.println("===============================================================================");
+		     // break;
+			   }
 		}
 	       
-	       
-	         
-	     
+	    for (Aluno aluno : alunos) {
+			System.out.println(" Alunos que sobraram na lista");
+			System.out.println(aluno.getNome());
+			System.out.println("Suas matérias:");
+			
+			
+			for (Disciplina disciplina : aluno.getDisciplinas()) {
+				System.out.println(disciplina.getDisciplina());		
+		}  
+	      */
+		 
+		   // Lista para pecorre por posicao alunos
+/*	    for(int pos =0 ; pos < alunos.size(); pos++) {
+	    	
+	    	 Aluno aluno = alunos.get(pos);
+	    	 
+	    	 //Substituição de um objeto dentro da lista
+	    	 if(aluno.getNome().equalsIgnoreCase("Mauricio")){
+	    		 
+	    		 Aluno trocar = new Aluno();
+	    		 trocar.setNome("aluno trocado");
+	    		 
+	    		 Disciplina disciplina = new Disciplina();
+	    		 disciplina.setDisciplina("Matematica");
+	    		 disciplina.setNota(9);
+	    		 
+	    		 
+	    		 trocar.getDisciplinas().add(disciplina);
+	    		 
+	    		 alunos.set(pos, trocar);
+	    		 
+	    		 aluno = alunos.get(pos);
+	    	 }
+	    			 
+	    	
+	    			 
+	    	 System.out.println("Aluno = "+ aluno.getNome());
+	    	 System.out.println("Media aluno = "+ aluno.getMediaNota());
+	    	 System.out.println("Resultado = "+ aluno.getAlunoAprovado2());
+	    	 System.out.println("--------------------------------------------------------");
+	    	 
           
-		
-		
+		// Lista para pecorre disciplina aluno
+	 /*   for (Disciplina disc : aluno.getDisciplinas()) {
+	     System.out.println("Materia = "+ disc.getDisciplina() +" Nota "+disc.getNota());
+	        
+	     	     }*/
+	     
+	     
+	     //Lista para pecorrer por posiçao de disciplina é a mesma coisa do for disciplina aluno
+	 /*    for(int posd = 0; posd < aluno.getDisciplinas().size() ; posd++ ) {
+	    	 Disciplina disc =aluno.getDisciplinas().get(posd);
+	    	 System.out.println("Materia = "+ disc.getDisciplina() +" Nota "+disc.getNota());
+	    	
+	     }
 		
 	}
-
+*/         
+		   //For para pecorre lista de alunos e separando por lista
+		   for(Aluno aluno : alunos) {
+			   
+			   // Se aluno for aprovado será adicionado na lista alunosAprovados
+			   if(aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.APROVADO)) {
+				   
+				   alunosAprovados.add(aluno);
+				   }
+			   //Senão se aluno ficar de recuperação será adicionado na lista alunosRecuperação
+			   else if(aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
+				   
+				   alunosRecuperacao.add(aluno);
+			   }
+			   //Senão se aluno for reprovado será adicionado na lista alunosReprovado
+			   else if(aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.REPROVADO)){
+				  alunosReprovados.add(aluno); 
+				  
+			   }
+			   		   
+				   
+		   }
+			   
+		   System.out.println("----------------Lista dos alunos Aprovados-------------------------");
+		   for(Aluno aluno : alunosAprovados) {
+			   
+			   System.out.println("Resultado =  "+ aluno.getNome()+ " com média de = "+aluno.getMediaNota()+" "+ aluno.getAlunoAprovado2());
+		   }
+             
+		   
+		   System.out.println("----------------Lista dos alunos Reprovados------------------------");
+		   for(Aluno aluno : alunosReprovados) {
+			   
+			   System.out.println("Resultado = "+ aluno.getNome()+ " com média de = "+aluno.getMediaNota()+" "+ aluno.getAlunoAprovado2());
+		   }
+		   
+		   
+		   System.out.println("----------------Lista dos alunos Recuperação-----------------------");
+		   for(Aluno aluno : alunosRecuperacao) {
+			   
+			   System.out.println("Resultado  = "+ aluno.getNome() +" com média de = "+aluno.getMediaNota()+" "+ aluno.getAlunoAprovado2());
+		   }
+	}
 }
