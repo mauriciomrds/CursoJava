5608802129cbd8aa2fb489ec069b4b6706035c18
lenovo-20 <lenovo-20@LAPTOP-VD6MJEJB>
2024-11-21 10:25:57 -0300

    Metodo extends da classe pai Pessoa

--- CursoJava/src/cursojava/classes/Secretario.java
+++ CursoJava/src/cursojava/classes/Secretario.java
@@ -3,6 +3,7 @@ package cursojava.classes;
 //Classe (filha) secretario herdando da classe (Pai) Pessoa
 public class Secretario extends Pessoa {
 
+	//Atributos da classe Secretario
 	private String registro;
 	private String nivelCargo;
 	private String experiencia;
@@ -24,11 +25,17 @@ public class Secretario extends Pessoa {
 	public void setExperiencia(String experiencia) {
 		this.experiencia = experiencia;
 	}
-	@Override
+	@Override //toString para facilitar a impressão dos atributos da classe Secretario
 	public String toString() {
 		return "Secretario [registro=" + registro + ", nivelCargo=" + nivelCargo + ", experiencia=" + experiencia
 				+ ", nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + "]";
+	}
+	@Override
+	//Metodo salario sub escrito da classe pai Pessoa heranca @Override
+	public double salario() {
+		
+		return 3000 * 0.9;
 	}
 	
 	
