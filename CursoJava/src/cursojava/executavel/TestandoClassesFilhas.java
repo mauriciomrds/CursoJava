c1091681f12959285e9728368eb18949dd3833dc
lenovo-20 <lenovo-20@LAPTOP-VD6MJEJB.mshome.net>
2025-08-06 15:29:12 -0300

    Atualização

--- CursoJava/src/cursojava/executavel/TestandoClassesFilhas.java
+++ CursoJava/src/cursojava/executavel/TestandoClassesFilhas.java
@@ -2,8 +2,10 @@ package cursojava.executavel;
 
 import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
+import cursojava.classes.Pessoa;
 import cursojava.classes.Secretario;
 
+//classe para imprimir dados do aluno, diretor e secretario
 public class TestandoClassesFilhas {
 	
 	public static void main(String[] args) {
@@ -38,9 +40,23 @@ public class TestandoClassesFilhas {
 // Se tentar estanciar o objeto Pessoa o java não vai deixar acessar o objeto direto, pois está  no padrão abstract 
 // exemplo: Pessoa pessoa = new Pessoa();
 	
-	System.out.println("Salario Aluno: "+ aluno.salario());
-	System.out.println("Salario Diretor: "+ diretor.salario());
-	System.out.println("Salario Secretario: "+ secretario.salario());
+	System.out.println("Salário Aluno: "+ aluno.salario());
+	System.out.println("Salário Diretor: "+ diretor.salario());
+	System.out.println("Salário Secretario: "+ secretario.salario());
 
+   //teste para retornar aluno, diretor e secretário, pois todos são pessoas isso é poliformismo
+	teste(aluno);
+	teste(diretor);
+	teste(secretario);
+	
+	//Polimorfismos é extensão das outras classe, Aluno é uma pessoa, Secretário é uma pessoa e Diretor é uma pessoa
+	Pessoa pessoa = new Aluno();
+	pessoa = secretario;
 }
+	
+	//teste chamando pessoa  e também invocando o metodo abstrato salario da classe Pai Pessoa
+	public static void teste(Pessoa pessoa) {
+		System.out.println("Essa pessoa é demais: "+ pessoa.getNome()+
+				" e o salário é de: "+ pessoa.salario());
+	}
 }
